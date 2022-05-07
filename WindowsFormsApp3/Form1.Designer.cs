64cc0cfeff6b4cc7091dbb19b4c0a3261ecbb735
Егор <lukin-egor-6616@edu.vivt.ru>
2022-05-07 21:12:49 +0300

    firstcoment

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox000 = new System.Windows.Forms.TextBox();
+            this.textBox00 = new System.Windows.Forms.TextBox();
+            this.textBox0 = new System.Windows.Forms.TextBox();
+            this.label0 = new System.Windows.Forms.Label();
+            this.label00 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.labelt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(365, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox000
+            // 
+            this.textBox000.Location = new System.Drawing.Point(13, 38);
+            this.textBox000.Name = "textBox000";
+            this.textBox000.Size = new System.Drawing.Size(196, 20);
+            this.textBox000.TabIndex = 2;
+            this.textBox000.TextChanged += new System.EventHandler(this.textBox000_TextChanged);
+            // 
+            // textBox00
+            // 
+            this.textBox00.Location = new System.Drawing.Point(300, 38);
+            this.textBox00.Name = "textBox00";
+            this.textBox00.Size = new System.Drawing.Size(60, 20);
+            this.textBox00.TabIndex = 3;
+            // 
+            // textBox0
+            // 
+            this.textBox0.Location = new System.Drawing.Point(450, 38);
+            this.textBox0.Name = "textBox0";
+            this.textBox0.Size = new System.Drawing.Size(60, 20);
+            this.textBox0.TabIndex = 4;
+            // 
+            // label0
+            // 
+            this.label0.AutoSize = true;
+            this.label0.Location = new System.Drawing.Point(230, 41);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(35, 13);
+            this.label0.TabIndex = 5;
+            this.label0.Text = "label1";
+            // 
+            // label00
+            // 
+            this.label00.AutoSize = true;
+            this.label00.Location = new System.Drawing.Point(380, 41);
+            this.label00.Name = "label00";
+            this.label00.Size = new System.Drawing.Size(35, 13);
+            this.label00.TabIndex = 6;
+            this.label00.Text = "label2";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(12, 85);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(35, 13);
+            this.label.TabIndex = 7;
+            this.label.Text = "label3";
+            // 
+            // labelt
+            // 
+            this.labelt.AutoSize = true;
+            this.labelt.Location = new System.Drawing.Point(13, 13);
+            this.labelt.Name = "labelt";
+            this.labelt.Size = new System.Drawing.Size(35, 13);
+            this.labelt.TabIndex = 8;
+            this.labelt.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 111);
+            this.Controls.Add(this.labelt);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label00);
+            this.Controls.Add(this.label0);
+            this.Controls.Add(this.textBox0);
+            this.Controls.Add(this.textBox00);
+            this.Controls.Add(this.textBox000);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Калькулятор расчета сопротивления электроцепи.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox000;
+        private System.Windows.Forms.TextBox textBox00;
+        private System.Windows.Forms.TextBox textBox0;
+        private System.Windows.Forms.Label label0;
+        private System.Windows.Forms.Label label00;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label labelt;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label0.Text = "Амперы(I)";
+            label00.Text = "Вольты(U)";
+            label.Text = "Результат: ";
+            labelt.Text = "Введите потребление напряжение(U) и силы тока(I) для каждого электроприбора.";
+            button1.Text = "Вычислить";
+            button2.Text = "Добавить элемент";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int y = 88, yf = 160, yb = 80, yl = 91, i = 1;
+
+        private void textBox000_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label.Text = "Результат: ";
+            Form f = ((Button)sender).FindForm();
+            for (int n = 0; n < i; n++)//0...23
+            {
+                    string s = Convert.ToString(n);
+                    TextBox tb2 = (TextBox)(from t in this.Controls.OfType<TextBox>() where t.Name == "textBox" + s select t).First();
+                    string st2 = tb2.Text;
+                    TextBox tb1 = (TextBox)(from t in this.Controls.OfType<TextBox>() where t.Name == "textBox" + s + s select t).First();
+                    string st1 = tb1.Text;
+                    label.Text = label.Text + Convert.ToString(Class1.R(Convert.ToDouble(st1), Convert.ToDouble(st2)));
+               
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s = Convert.ToString(i);
+            TextBox text = new TextBox();yb += 50;
+            text.Location = new Point(13, y);
+            text.Size = new Size(196, 20);
+            text.Name = "textBox" + s + s + s;
+            this.Controls.Add(text);
+            text = new TextBox();
+            text.Location = new Point(300, y);
+            text.Size = new Size(60, 20);
+            text.Name = "textBox" + s + s;
+            this.Controls.Add(text);
+            text = new TextBox();
+            text.Location = new Point(450, y);
+            text.Size = new Size(60, 20);
+            text.Name = "textBox" + s;
+            this.Controls.Add(text);
+            Label label = new Label();
+            label.Location = new Point(230, yl);
+            label.Name = "textBox" + s;
+            label.Text = "Амперы(I)";
+            this.Controls.Add(label); 
+            label = new Label();
+            label.Location = new Point(380, yl);
+            label.Name = "textBox" + s + s;
+            label.Text = "Вольты(U)";
+            this.Controls.Add(label);
+            this.button1.Location = new Point(275,yb);
+            this.button2.Location = new Point(365,yb);
+            this.label.Location = new Point(12,yb+5);
+            this.ClientSize = new Size(525,yf); yf += 50;y += 50; yl += 50;
+        }
+    }
+}
